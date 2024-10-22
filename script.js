fbb1174572de4bb0ce6ec5ffc76cfe03eede0e3a
FrancisB143 <fbangoy_230000001354@uic.edu.ph>
2024-10-22 23:41:40 +0800

    v3.1 dark-mode
    -dark_mode/light_mode

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+var icon = document.getElementById("icon");
+
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        icon.src = "images/light_mode.png";
+    } else{
+        icon.src = "images/dark_mode.png";
+    }
+}
