2f7f7a550a253767b82cfd662c2f82c86d07cc99
FrancisB143 <fbangoy_230000001354@uic.edu.ph>
2024-11-17 00:39:44 +0800

    v4 big changes
    -deleted favorites
    -deleted extras
    -added talent section
    -update project section

--- script.js
+++ script.js
@@ -15,3 +15,27 @@ icon.onclick = function(){
         icon.src = "images/dark_mode.png";
     }
 }
+
+const slider = document.querySelector(".slider");
+const slides = document.querySelectorAll(".slide");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+let slideIndex = 0;
+slides[slideIndex].classList.add('active');
+prevBtn.addEventListener('click', prevSlide);
+nextBtn.addEventListener('click', nextSlide);
+
+function prevSlide(){
+    slides[slideIndex].classList.remove('active');
+    slideIndex = (slideIndex === 0) ? slides.length - 1 : slideIndex - 1;
+    slides[slideIndex].classList.add('active');
+    slider.style.transform = `translateX(-${slideIndex * 100}%)`;
+}
+
+function nextSlide(){
+    slides[slideIndex].classList.remove('active');
+    slideIndex = (slideIndex === slides.length - 1) ? 0 : slideIndex + 1;
+    slides[slideIndex].classList.add('active');
+    slider.style.transform = `translateX(-${slideIndex * 100}%)`;
+}
